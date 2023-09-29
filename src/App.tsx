2a3d1053605b951be0ee930ac86a7daaaa8ad46f
KASAYED <kasayeddev@gmail.com>
2023-09-28 23:40:48 -0600

    Handle games with no Image

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
-import { GameQuery } from './App';
+
 
 export interface GameQuery {
 	genre: Genre | null;
